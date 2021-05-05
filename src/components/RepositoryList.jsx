c64d41f4f9ef04e46fd9174f75bd6471e4100e55
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-04 21:42:16 -0300

    first component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>List de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
